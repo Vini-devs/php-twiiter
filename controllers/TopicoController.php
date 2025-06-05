9a67e9b8871b2adf3fa10fc026ecf6540bfda3fc
cntoliveira <canotdev@gmail.com>
2025-06-05 00:59:13 -0300

    Criacao dos files topicos dentro da view e alteracao nos controllers e models

---
+++ controllers/TopicoController.php
@@ -0,0 +1,67 @@
+<?php
+require_once __DIR__ . '/../models/topico.php';
+
+class TopicoController {
+    static function encontrarTopicos() {
+        $topicos = Topico::encontrarTopicos();
+        include __DIR__ . '/../views/topicos/topicos.php';
+    }
+
+    static function criarTopico() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nome = $_POST['nome'] ?? null;
+
+            if (!$nome) {
+                echo "<br>Erro: informe um nome para o tópico.";
+            } else {
+                $sucesso = Topico::criarTopico($nome);
+                if ($sucesso) {
+                    header("Location: /php-twitter/topico");
+                    exit;
+                } else {
+                    echo "<br>Erro ao criar tópico. Tente novamente.";
+                }
+            }
+        }
+
+        include __DIR__ . '/../views/topicos/criar-topico.php';
+    }
+
+    static function editarTopico($idTopico) {
+        $topico = Topico::encontrarTopicoPorId($idTopico);
+
+        if (!$topico) {
+            echo "<br>Tópico não encontrado.";
+            return;
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nome = $_POST['nome'] ?? null;
+
+            if (!$nome) {
+                echo "<br>Erro: informe um novo nome.";
+            } else {
+                $sucesso = Topico::editarTopico($idTopico, $nome);
+                if ($sucesso) {
+                    header("Location: /php-twitter/topico");
+                    exit;
+                } else {
+                    echo "<br>Erro ao editar tópico. Tente novamente.";
+                }
+            }
+        }
+
+        include __DIR__ . '/../views/topicos/editar-topico.php';
+    }
+
+    static function apagarTopico($idTopico) {
+        $sucesso = Topico::apagarTopico($idTopico);
+
+        if ($sucesso) {
+            header("Location: /php-twitter/topico");
+            exit;
+        } else {
+            echo "<br>Erro ao apagar tópico. Tente novamente.";
+        }
+    }
+}
--- index.php
+++ index.php
@@ -15,6 +15,7 @@ require __DIR__ . '/controllers/AuthController.php';
 require __DIR__ . '/controllers/PostController.php';
 require __DIR__ . '/controllers/RespostaController.php';
 require __DIR__ . '/controllers/MensagemController.php';
+require __DIR__ . '/controllers/TopicoController.php';
 
 match ($pagina) {
     'login'                     => AuthController::login(),
@@ -35,10 +36,10 @@ match ($pagina) {
     'resposta/editar'           => RespostaController::editarResposta($url[2]),
     'resposta/apagar'           => RespostaController::apagarResposta($url[2]),
 
-    // 'topico'                 => TopicoController::encontrarTopico($url[2]),
-    // 'topico/criar'           => TopicoController::criarTopico($url[2]),
-    // 'topico/editar'          => TopicoController::editarTopico($url[2]),
-    // 'topico/apagar'          => TopicoController::apagarTopico($url[2]),
+    'topico'                 => TopicoController::encontrarTopico($url[2]),
+    'topico/criar'           => TopicoController::criarTopico($url[2]),
+    'topico/editar'          => TopicoController::editarTopico($url[2]),
+    'topico/apagar'          => TopicoController::apagarTopico($url[2]),
 
     'mensagem'                 => MensagemController::encontrarMensagem($url[1] ?? null),
     // 'mensagem/criar'           => MensagemController::criarMensagem($url[2]),
